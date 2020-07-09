0e3e0e9869e1100149a0ccd62ff70130e73c32d7
Administrator <1171969053@qq.com>
2020-07-09 15:12:29 +0800

    新增experiment2,这才是作业

---
+++ src/main/java/experiment2/Job.java
@@ -0,0 +1,132 @@
+package experiment2;
+
+/**
+ * @author Administrator
+ */
+public class Job{
+    private String name;
+    private double weight;
+    private int arriveTime;
+    private int needTime;
+    private int runningTime;
+    private int finishTime;
+    private int waiteTime;
+    private int turnaroundTime;
+    private double weightedTurnaroundTime;
+    private boolean isFinish = false;
+
+    public Job() {
+    }
+
+    public Job(String name, int weight, int arriveTime, int needTime) {
+        this.name = name;
+        this.weight = weight;
+        this.arriveTime = arriveTime;
+        this.needTime = needTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getArriveTime() {
+        return arriveTime;
+    }
+
+    public void setArriveTime(int arriveTime) {
+        this.arriveTime = arriveTime;
+    }
+
+    public int getNeedTime() {
+        return needTime;
+    }
+
+    public void setNeedTime(int needTime) {
+        this.needTime = needTime;
+    }
+
+    public int getRunningTime() {
+        return runningTime;
+    }
+
+    public void setRunningTime(int runningTime) {
+        this.runningTime = runningTime;
+    }
+
+    public int getWaiteTime() {
+        return waiteTime;
+    }
+
+    public void setWaiteTime(int waiteTime) {
+        this.waiteTime = waiteTime;
+    }
+
+    public int getFinishTime() {
+        return finishTime;
+    }
+
+    public void setFinishTime(int finishTime) {
+        this.finishTime = finishTime;
+        //更新周转时间
+        //周转时间=完成时间-到达时间
+        setTurnaroundTime(finishTime - getArriveTime());
+    }
+
+    public int getTurnaroundTime() {
+        return turnaroundTime;
+    }
+
+    public void setTurnaroundTime(int turnaroundTime) {
+        this.turnaroundTime = turnaroundTime;
+        //设置带权周转时间
+        //带权周转时间=周转时间/服务时间
+        weightedTurnaroundTime = 1.0 * turnaroundTime/needTime;
+    }
+
+    public double getWeightedTurnaroundTime() {
+        return weightedTurnaroundTime;
+    }
+
+    public void setWeightedTurnaroundTime(double weightedTurnaroundTime) {
+        this.weightedTurnaroundTime = weightedTurnaroundTime;
+    }
+
+    public boolean isFinish() {
+        return isFinish;
+    }
+
+    public void setFinish(boolean finish) {
+        isFinish = finish;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "Job{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                ", arriveTime=" + arriveTime +
+                ", needTime=" + needTime +
+                ", runningTime=" + runningTime +
+                ", finishTime=" + finishTime +
+                ", turnaroundTime=" + turnaroundTime +
+                ", weightedTurnaroundTime=" + weightedTurnaroundTime +
+                '}';
+    }
+}
---
+++ src/main/java/experiment2/Main.java
@@ -0,0 +1,103 @@
+package experiment2;
+
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author Administrator
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        List<Job> jobs = null;
+        //录入进程数据
+        A:while(true){
+            System.out.println("请输入进程名、优先权、到达时间、需要运行时间");
+            String msg = input.nextLine();
+            //进程名
+            String name = msg.substring(0,msg.indexOf(" "));
+            //优先数
+            msg = msg.substring(msg.indexOf(" ") + 1);
+            int weight = Integer.valueOf(msg.substring(0, msg.indexOf(" ")));
+            //到达时间
+            msg = msg.substring(msg.indexOf(" ") + 1);
+            int arriveTime = Integer.valueOf(msg.substring(0, msg.indexOf(" ")));
+            //需要运行时间
+            msg = msg.substring(msg.indexOf(" ") + 1);
+            int needTime = Integer.valueOf(msg);
+            //新建job
+            Job job = new Job(name, weight, arriveTime, needTime);
+            //存入job
+            Algorithm.addJob(job);
+            //是否继续
+            System.out.println("是否继续录入:1/继续;2/结束");
+            while(true) {
+                String is = input.next();
+                input.nextLine();
+                if ("1".equals(is)) {
+                    continue A;
+                } else if ("2".equals(is)) {
+                    break A;
+                } else {
+                    System.out.println("指令输入有误,请重新输入");
+                    continue;
+                }
+            }
+        }
+        //选择算法
+        B:while(true){
+            System.out.println("请选择算法:1/先来先服务;2/最短作业优先;3/响应比高者优先;4/优先权优先;5/简单轮转法");
+            String string = input.next();
+            //先来先服务
+            if ("1".equals(string)){
+                jobs = Algorithm.FCFS();
+                break;
+            }else if("2".equals(string)){    //对短作业优先
+                jobs = Algorithm.SJF();
+                break;
+            }else if("3".equals(string)){   //响应高者比优先
+                jobs = Algorithm.HRN();
+                break;
+            }else if("4".equals(string)){   //优先权优先
+                try {
+                    jobs = Algorithm.WEIGHT();
+                    break;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }else if("5".equals(string)){   //简单轮转法
+                System.out.println("请输入时间片大小");
+                int q = input.nextInt();
+                jobs = Algorithm.RR(q);
+                break;
+            }else{
+                System.out.println("输入指令有误,请重新输入");
+                continue B;
+            }
+        }
+        //打印最终的运行结果
+        //运行结束
+        System.out.println("运行结束");
+        //打印信息
+        System.out.println("进程名\t到达时间\t服务时间\t完成时间\t优先权\t周转时间\t带权周转时间\t");
+        for (Job job:jobs) {
+            System.out.println(job.getName() + "   \t "
+                    + job.getArriveTime() + "   \t "
+                    + job.getNeedTime() + "   \t "
+                    + job.getFinishTime() + "   \t "
+                    + job.getWeight() + "    \t"
+                    + job.getTurnaroundTime() + "   \t "
+                    +job.getWeightedTurnaroundTime() + "   \t");
+        }
+        double averageTurnaroundTime = 0;
+        double averageWeightedTurnaroundTime = 0;
+        for (Job job:jobs) {
+            averageTurnaroundTime += job.getTurnaroundTime();
+            averageWeightedTurnaroundTime += job.getWeightedTurnaroundTime();
+        }
+        averageTurnaroundTime /= jobs.size();
+        averageWeightedTurnaroundTime /= jobs.size();
+        System.out.println("平均周转时间: " + averageTurnaroundTime);
+        System.out.println("平均带权周转时间: " + averageWeightedTurnaroundTime);
+    }
+}
